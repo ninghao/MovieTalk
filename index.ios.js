7f095474642dad7d7f1a31f9db267ad073a1323b
wanghao <wanghao@dongxuedeMacBook.local>
2015-09-28 07:37:41 +0800

    组件应用的样式

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,7 @@
 'use strict';
 
 import React from 'react-native';
+import styles from './app/Styles/Main';
 
 let {
   AppRegistry,
@@ -85,84 +86,6 @@ class MovieTalk extends React.Component {
     );
   }
 }
-
-let styles = StyleSheet.create({
-  item: {
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderColor: 'rgba(100, 53, 201, 0.1)',
-    paddingBottom: 6,
-    marginBottom: 6,
-    flex: 1,
-  },
-  itemContent: {
-    flex: 1,
-    marginLeft: 13,
-    marginTop: 6,
-  },
-  itemHeader: {
-    fontSize: 18,
-    fontFamily: 'Helvetica Neue',
-    fontWeight: '300',
-    color: '#6435c9',
-    marginBottom: 6,
-  },
-  itemMeta: {
-    fontSize: 16,
-    color: 'rgba(0, 0, 0, 0.6)',
-    marginBottom:6,
-  },
-  redText: {
-    color: '#db2828',
-    fontSize: 15,
-  },
-  loading: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.3)',
-    alignItems: 'center',
-  },
-  overlayHeader: {
-    fontSize: 33,
-    fontFamily: 'Helvetica Neue',
-    fontWeight: '200',
-    color: '#eae7ff',
-    padding: 10,
-  },
-  overlaySubHeader: {
-    fontSize: 16,
-    fontFamily: 'Helvetica Neue',
-    fontWeight: '200',
-    color: '#eae7ff',
-    padding: 10,
-    paddingTop: 0,
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-  },
-  image: {
-    width: 99,
-    height: 138,
-    margin: 6,
-  },
-  itemText: {
-    fontSize: 33,
-    fontFamily: 'Helvetica Neue',
-    fontWeight: '200',
-    color: '#6435c9',
-    padding: 30,
-  },
-  container: {
-    flexDirection: 'row',
-    backgroundColor: '#eae7ff',
-    flex: 1,
-    paddingTop: 23,
-  }
-});
 
 AppRegistry.registerComponent('MovieTalk', () => MovieTalk);
 
