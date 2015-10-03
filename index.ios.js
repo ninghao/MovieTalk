95224e229e9a5495a85ca906ed3fafbc2835c1e6
wanghao <wanghao@dongxuedeMacBook.local>
2015-10-03 14:46:11 +0800

    切换显示标签项目

--- index.ios.js
+++ index.ios.js
@@ -23,15 +23,32 @@ const REQUEST_URL = 'https://api.douban.com/v2/movie/top250';
 class MovieTalk extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedTab: 'us_box'
+    };
   }
 
   render() {
     return (
       <TabBarIOS>
-        <TabBarIOS.Item systemIcon="featured">
+        <TabBarIOS.Item
+          systemIcon="featured"
+          selected={this.state.selectedTab === 'featured'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'featured'
+            });
+          }}>
           <MovieList />
         </TabBarIOS.Item>
-        <TabBarIOS.Item systemIcon="most-viewed" selected="true">
+        <TabBarIOS.Item
+          systemIcon="most-viewed"
+          selected={this.state.selectedTab === 'us_box'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'us_box'
+            });
+          }}>
           <USBox />
         </TabBarIOS.Item>
       </TabBarIOS>
