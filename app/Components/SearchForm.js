07eabd09271ca01cb86b13f76863519b582e4978
wanghao <117663444@qq.com>
2015-10-29 16:16:43 +0800

    文本框的样式

--- app/Components/SearchForm.js
+++ app/Components/SearchForm.js
@@ -22,10 +22,18 @@ class SearchForm extends React.Component {
   render() {
     return (
       <View style={[styles.container, {paddingTop: 60}]}>
+        <View style={{
+          paddingTop: 7,
+          paddingLeft: 7,
+          paddingRight: 7,
+          borderColor: "rgba(100, 53, 201, 0.1)",
+          borderBottomWidth: 1,
+        }}>
         <TextInput
           style={{height: 50}}
           placeholder="搜索 ..."
          />
+         </View>
       </View>
     );
   }
